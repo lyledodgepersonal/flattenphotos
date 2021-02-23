9100e0fc6b63622e8000b6296d50150b0377d476
Lyle Dodge <lyle@thedodgefamily.net>
2021-02-23 12:57:36 -0800

    initial commit, baseline working without much error handling

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,50 @@ namespace flattenphotos
 {
     class Program
     {
+        static List<string> copiedFiles = new List<string>();
+        static List<string> ignoredFiles = new List<string>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine(Environment.CurrentDirectory);
+
+            if (args.Length < 2)
+                Console.WriteLine("Need two directories!");
+
+            DirectoryInfo source = new DirectoryInfo(args[0]);
+            DirectoryInfo destination = new DirectoryInfo(args[1]);
+            string rootPath = source.FullName;
+            ProcessDirectory(rootPath, source, destination);
+            Console.WriteLine("Copied Files : {0}", copiedFiles.Count);
+            Console.WriteLine("Ignored Files : {0}", ignoredFiles.Count);
+        }
+
+        static void ProcessDirectory(string rootPath, DirectoryInfo source, DirectoryInfo destination)
+        {
+            foreach (DirectoryInfo directory in source.GetDirectories())
+            {
+                ProcessDirectory(rootPath, directory, destination);
+            }
+
+            foreach(FileInfo file in source.GetFiles())
+            {
+                string fullName = file.FullName;
+                string simpleDestinationName = fullName.Replace(rootPath, "").Substring(1).Replace(@"\", "-");
+                string fullDestinationName = Path.Combine(destination.FullName, simpleDestinationName);
+
+                if (fullDestinationName.Contains(".jpg") || fullDestinationName.Contains(".jpeg") || fullDestinationName.Contains(".png"))
+                {
+                    if (File.Exists(fullDestinationName))
+                    {
+                        ignoredFiles.Add(fullDestinationName);
+                    }
+                    else
+                    {
+                        file.CopyTo(fullDestinationName);
+                        copiedFiles.Add(fullDestinationName);
+                    }
+                }
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Overview
+
+iTunes doesn't like nested directories for photo sharing to AppleTV, so I created this project to make it easy to take an input folder (I filter by jpg, jpeg, png) and flatten it (directory-[subdirectories]-filename) into one folder, so that you can sign in to iTunes, turn on home sharing, then pick the destination folder.
+
+It just checks for existence or not, and copies sources over, not any file modifications.
+
+Usage: flattenphotos "source directory" "destination directory"
